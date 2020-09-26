097744767a6f1048bec8fe3cb1ca5d24e95195e5
Edward Leung <edward_leung_12@hotmail.com>
2020-09-27 00:37:27 +0100

    added extra elements from api to component

--- my-app/src/components/JobBoardComponent.js
+++ my-app/src/components/JobBoardComponent.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 const JobBoardComponent = ({ job }) => (
-  <div className='flex'>
+  <div className='flex bg-white shadow-md m-4 p-4'>
     <div>
       <img src={job.logo} alt={job.company} />
     </div>
-    <div></div>
+    <div className='ml-2'>
+      <h3>{job.company}</h3>
+      <h2>{job.position}</h2>
+      <p>
+        {job.postedAt} · {job.contract} · {job.location}
+      </p>
+    </div>
     <div></div>
   </div>
 );
