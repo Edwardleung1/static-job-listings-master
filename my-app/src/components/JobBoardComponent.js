98785290ffdd723c47a24253c6e9769be08fb3f2
Edward Leung <edward_leung_12@hotmail.com>
2020-09-27 00:13:54 +0100

    creating job component

---
+++ my-app/src/components/JobBoardComponent.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const JobBoardComponent = ({ job }) => (
+  <div>
+    <h1 className='text-3xl'>{job.company}</h1>
+    <p>Hi</p>
+  </div>
+);
+
+export default JobBoardComponent;
